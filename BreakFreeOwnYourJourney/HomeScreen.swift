0be9d25daa0ac357fed68ed6063ad13de07ba007
Harshit Agarwal <harshitagarwal46446@icloud.com>
2024-03-08 01:40:19 +0530

    working with sample data and establishing flow ctrl

--- BreakFreeOwnYourJourney/HomeScreen.swift
+++ BreakFreeOwnYourJourney/HomeScreen.swift
@@ -9,6 +9,24 @@ import SwiftUI
 import SwiftData
 import Charts
 
+var sampleData: [PuffTrackingData] = [
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 13)), numberOfPuff: 10, nicotineStrength: 15.6, puffDurationInMinutes: 5, additionalNotes: ""),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 12)), numberOfPuff: 12, nicotineStrength: 12.0, puffDurationInMinutes: 3, additionalNotes: "Feeling stressed at work"),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 11)), numberOfPuff: 8, nicotineStrength: 15.6, puffDurationInMinutes: 4, additionalNotes: ""),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 10)), numberOfPuff: 15, nicotineStrength: 18.0, puffDurationInMinutes: 7, additionalNotes: "Craving after dinner"),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 9)), numberOfPuff: 7, nicotineStrength: 12.0, puffDurationInMinutes: 2, additionalNotes: ""),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 8)), numberOfPuff: 11, nicotineStrength: 15.6, puffDurationInMinutes: 6, additionalNotes: "With friend who vapes"),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 7)), numberOfPuff: 5, nicotineStrength: 12.0, puffDurationInMinutes: 3, additionalNotes: ""),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 6)), numberOfPuff: 9, nicotineStrength: 15.6, puffDurationInMinutes: 4, additionalNotes: "Feeling anxious"),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 5)), numberOfPuff: 6, nicotineStrength: 12.0, puffDurationInMinutes: 2, additionalNotes: ""),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 4)), numberOfPuff: 13, nicotineStrength: 18.0, puffDurationInMinutes: 8, additionalNotes: "Social gathering with vapers"),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 3)), numberOfPuff: 8, nicotineStrength: 15.6, puffDurationInMinutes: 4, additionalNotes: ""),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 2)), numberOfPuff: 15, nicotineStrength: 18.0, puffDurationInMinutes: 7, additionalNotes: "Craving after dinner"),
+  PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 1)), numberOfPuff: 7, nicotineStrength: 12.0, puffDurationInMinutes: 2, additionalNotes: ""),
+  PuffTrackingData(date: Date(), numberOfPuff: 19, nicotineStrength: 15.6, puffDurationInMinutes: 6, additionalNotes: "With friend who vapes"),
+  PuffTrackingData(date: Date().addingTimeInterval((60 * 60 * 24 * 1)), numberOfPuff: 0, nicotineStrength: 0, puffDurationInMinutes: 0, additionalNotes: "")
+]
+
 struct HomeScreen: View {
     @AppStorage("background") var backgroundImage = "bg3"
     @AppStorage("Name") var name: String = ""
@@ -16,23 +34,30 @@ struct HomeScreen: View {
     @Environment(\.modelContext) var modelContext
     
     @Query var puffTracking: [PuffTrackingData]
+        
+    var totalPuffs: Int {
+            sampleData.reduce(0) { $0 + $1.numberOfPuff }
+        }
     
-    var sampleData: [PuffTrackingData] = [
-      PuffTrackingData(date: Date(), numberOfPuff: 10, nicotineStrength: 15.6, puffDurationInMinutes: 5, additionalNotes: ""),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24)), numberOfPuff: 12, nicotineStrength: 12.0, puffDurationInMinutes: 3, additionalNotes: "Feeling stressed at work"),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 2)), numberOfPuff: 8, nicotineStrength: 15.6, puffDurationInMinutes: 4, additionalNotes: ""),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 3)), numberOfPuff: 15, nicotineStrength: 18.0, puffDurationInMinutes: 7, additionalNotes: "Craving after dinner"),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 4)), numberOfPuff: 7, nicotineStrength: 12.0, puffDurationInMinutes: 2, additionalNotes: ""),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 5)), numberOfPuff: 11, nicotineStrength: 15.6, puffDurationInMinutes: 6, additionalNotes: "With friend who vapes"),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 6)), numberOfPuff: 5, nicotineStrength: 12.0, puffDurationInMinutes: 3, additionalNotes: ""),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 7)), numberOfPuff: 9, nicotineStrength: 15.6, puffDurationInMinutes: 4, additionalNotes: "Feeling anxious"),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 8)), numberOfPuff: 6, nicotineStrength: 12.0, puffDurationInMinutes: 2, additionalNotes: ""),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 9)), numberOfPuff: 13, nicotineStrength: 18.0, puffDurationInMinutes: 8, additionalNotes: "Social gathering with vapers"),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 10)), numberOfPuff: 8, nicotineStrength: 15.6, puffDurationInMinutes: 4, additionalNotes: ""),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 11)), numberOfPuff: 15, nicotineStrength: 18.0, puffDurationInMinutes: 7, additionalNotes: "Craving after dinner"),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 12)), numberOfPuff: 7, nicotineStrength: 12.0, puffDurationInMinutes: 2, additionalNotes: ""),
-      PuffTrackingData(date: Date().addingTimeInterval(-(60 * 60 * 24 * 13)), numberOfPuff: 11, nicotineStrength: 15.6, puffDurationInMinutes: 6, additionalNotes: "With friend who vapes")
-    ]
+    func salesInPeriod(in range: ClosedRange<Date>) -> Int {
+//        sampleData.filter { range.contains($0.day) }.reduce(0) { $0 + $1.sales }
+        sampleData.filter { range.contains($0.date) }.reduce(0) { $0 + $1.numberOfPuff}
+    }
+    
+    
+    @State var scrollPositionStart: Date = sampleData.last!.date.addingTimeInterval( -1 * 3600 * 24 * 7)
+    
+    var scrollPositionEnd: Date {
+        scrollPositionStart.addingTimeInterval(3600 * 24 * 7)
+    }
+    
+    var scrollPositionString: String {
+        scrollPositionStart.formatted(.dateTime.month().day())
+    }
+    
+    var scrollPositionEndString: String {
+        scrollPositionEnd.addingTimeInterval(-(3600 * 24 * 1)).formatted(.dateTime.month().day().year())
+    }
     
     var body: some View {
         NavigationStack{
@@ -40,36 +65,84 @@ struct HomeScreen: View {
                 Image(backgroundImage)
                     .resizable()
                     .opacity(0.7)
-                VStack{
+                VStack(alignment: .leading){
+                    Text(name)
+                        .font(.largeTitle.bold())
+                        .padding([.leading])
                     ZStack{
                         Rectangle()
                             .frame(height: 300)
-                            .foregroundColor(Color.black)
-                            .opacity(0.4)
+                            .foregroundColor(Color.blue)
+                            .opacity(0.2)
                         VStack(alignment: .leading){
-                            Text("Number of puff")
+                            Text("Number of puff: \(salesInPeriod(in: scrollPositionStart...scrollPositionEnd))")
+                                .font(.title2.bold())
+                                .foregroundColor(.primary)
+                            
+                            Text("\(scrollPositionString) – \(scrollPositionEndString)")
+                                .font(.callout)
+                                .foregroundStyle(.secondary)
+                            
                             Chart(sampleData){ puff in
                                 BarMark(
                                     x: .value("Day", puff.date,unit: .day),
-                                    y: .value("Number of puff", puff.numberOfPuff)
+                                    y: .value("Number of puff", puff.numberOfPuff),
+                                    width: .ratio(0.6)
                                 )
                             }
                             .chartScrollableAxes(.horizontal)
                             .chartXVisibleDomain(length: 3600 * 24 * 7 )
+                            .foregroundColor(.orange)
                             .chartXAxis {
                                 AxisMarks(values: .stride(by: .day, count: 1)) {
                                     AxisTick()
                                     AxisGridLine()
-                                    AxisValueLabel(format: .dateTime.weekday())
+                                    AxisValueLabel(format: .dateTime.month().day())
                                 }
                             }
+                            .chartScrollPosition(x: $scrollPositionStart)
+                            .frame(height: 240)
+                        }
+                        .padding()
+
+                    }
+                    ZStack{
+                        Rectangle()
+                            .frame(height: 300)
+                            .foregroundColor(Color.blue)
+                            .opacity(0.2)
+                        VStack(alignment: .leading){
+                            Text("Most Used Nicotine Concentration")
+                                .foregroundColor(.secondary)
+                            
+                            Text("\(scrollPositionString) – \(scrollPositionEndString)")
+                                .font(.callout)
+                                .foregroundStyle(.secondary)
+                            
+                            Chart(sampleData){ puff in
+                                BarMark(
+                                    x: .value("Day", puff.date,unit: .day),
+                                    y: .value("Number of puff", puff.numberOfPuff),
+                                    width: .ratio(0.6)
+                                )
+                            }
+                            .chartScrollableAxes(.horizontal)
+                            .chartXVisibleDomain(length: 3600 * 24 * 7 )
+                            .foregroundColor(.orange)
+                            .chartXAxis {
+                                AxisMarks(values: .stride(by: .day, count: 1)) {
+                                    AxisTick()
+                                    AxisGridLine()
+                                    AxisValueLabel(format: .dateTime.month().day())
+                                }
+                            }
+                            .chartScrollPosition(x: $scrollPositionStart)
                             .frame(height: 240)
                         }
                         .padding()
 
                     }
                 }
-                .navigationTitle(name)
             }
             .ignoresSafeArea(.all)
             .toolbar {
